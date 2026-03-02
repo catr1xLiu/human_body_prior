fa57e6cabc2e25241273fd539b2a8b6da06a2138
catr1x <y3275liu@uwaterloo.ca>
2026-03-02 17:16:56 -0500

    Incomplete work for humanml3d model export

---
+++ src/export_humanml3d_v2.py
@@ -0,0 +1,9 @@
+from human_body_prior.body_model.body_model import BodyModel
+
+import sys, os
+import torch
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+# TODO: Export SMPL Joints to HumanML3D format.
